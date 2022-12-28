29bc82e08b56ce0909d6d1177d651d184e9d797f
hvenanc <hvs@poli.br>
2022-12-28 06:27:36 -0300

    Commit de volta para branch main

--- Pratica Git/src/HelloWorld.java
+++ Pratica Git/src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Iniciando Prática 4");
+        System.out.println("Voltei para Branch main");
     }
 }
